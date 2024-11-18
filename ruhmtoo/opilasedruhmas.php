6f513755f6d947b3472ee302e79fde3de3fb0f10
synd3772 <doe481157@gmail.com>
2024-11-18 11:29:41 +0200

    different colors added for text about student hair color

--- ruhmtoo/opilasedruhmas.php
+++ ruhmtoo/opilasedruhmas.php
@@ -1,21 +1,34 @@
 <?php if (isset($_GET['code'])) {die(highlight_file(__FILE__, 1));}?>
 <?php
 $opilased = simplexml_load_file("xml/opilased.xml");
-function opilaneDiv($nimi, $juuksevarv, $sitelink, $gender){ // see funktsioon on vajalik konteineri loomiseks, mis sisaldab teavet inimese kohta
+function opilaneDiv($id, $nimi, $juuksevarv, $sitelink, $gender){ // see funktsioon on vajalik konteineri loomiseks, mis sisaldab teavet inimese kohta
     echo "<div class='opilane-card ".$gender."'>";
     echo "<div class='opilane-card-header'>";
     echo "<h3>Nimi: ".$nimi."</h3>";
 
     echo "</div>";
     echo "<div class='opilane-card-body'>";
-    echo "<p>Juuksuvärv: ".$juuksevarv."</p>";
+
+    echo "<p>Juuksuvärv: <span style='color:".getColorByHairColor($juuksevarv)."'>".$juuksevarv."</span></p>";
 
     echo "</div>";
     echo "<div class='opilane-card-footer'>";
     echo "<button class='leht-button' onclick=\"clickButton(this, '".$nimi."', '".$sitelink."')\">Tema leht</button>";
-//    echo "<form method='post'><input type='submit' class='kustuta-button' value='X' /></form>";
+    echo "<form method='post' class='kustuta-form-container'><input name='container-id' type='hidden' value='".$id."'><input type='submit' name='kustuta-button' value='X' class='kustuta-button'></form>";
     echo "</div>";
     echo "</div>";
+}
+function getColorByHairColor($hairColor){
+    switch(strtolower($hairColor)){
+        case "must":
+            return "#989898";
+        case "hele":
+            return "#BEB697";
+        case "punapea":
+            return "#BE9672";
+        default:
+            return "white";
+    }
 }
 ?>
 <?php
@@ -28,6 +41,7 @@ if(isset($_POST["submit"])) { // siin töötlame HTTP taotlus õpilase lisamisek
     $xml_root = $xmlDoc->createElement('opilane');
     $specificNode->appendChild($xml_root);
     if(!empty($_POST["name-input"]) && !empty($_POST["juuksuvarv-input"] && !empty($_POST["genders"]))){ // kontrollime kui meil on kõik teave
+        $xml_root->appendChild($xmlDoc->createElement('id', count($xmlDoc->getElementsByTagName('opilane'))));
         $xml_root->appendChild($xmlDoc->createElement("nimi", $_POST["name-input"]));
         $xml_root->appendChild($xmlDoc->createElement("juuksevarv", $_POST["juuksuvarv-input"]));
         if(!empty($_POST["site-link-input"])){
@@ -39,10 +53,37 @@ if(isset($_POST["submit"])) { // siin töötlame HTTP taotlus õpilase lisamisek
         $xml_root->appendChild($xmlDoc->createElement("gender", $_POST["genders"]));
         $xmlDoc->save('xml/opilased.xml');
 
-        exit(header('Location: opilasedruhmas.php?m=success')); // lõpetame protsessi, saates kasutaja tagasi lehele
+        exit(header('Location: opilasedruhmas.php')); // lõpetame protsessi, saates kasutaja tagasi lehele
     }
 }
+?>
+<?php
+if(isset($_POST["kustuta-button"])){
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->formatOutput = true;
+    $xmlDoc->preserveWhiteSpace = false;
+    $xmlDoc->load("xml/opilased.xml");
+    $opilane_nodes = $xmlDoc->getElementsByTagName("opilane");
+    $found_node = null;
+    $break_all = false;
+    foreach($opilane_nodes as $node){
+        if($break_all){
+            break;
+        }
+        foreach($node->childNodes as $child){
+            if($child->nodeName == "id"){
+                if($child->nodeValue === $_POST["container-id"]){
+                    $node->remove();
+                    $break_all = true;
+                    break;
+                }
+            }
+        }
+    }
+    $xmlDoc->save('xml/opilased.xml');
 
+    exit(header('Location: opilasedruhmas.php')); // lõpetame protsessi, saates kasutaja tagasi lehele
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -69,7 +110,7 @@ if(isset($_POST["submit"])) { // siin töötlame HTTP taotlus õpilase lisamisek
             echo "<div class='opilane-row'>";
             $first = true;
         }
-        opilaneDiv($opilane->nimi, $opilane->juuksevarv, $opilane->sitelink, $opilane->gender); // kutsume funktsiooni konteinerid loomiseks
+        opilaneDiv($opilane->id, $opilane->nimi, $opilane->juuksevarv, $opilane->sitelink, $opilane->gender); // kutsume funktsiooni konteinerid loomiseks
         if($row_count === $max_size_of_row || $index === count($opilased)){ // kontrollime kui see on lõpp ja siis paname div'i kinni
             echo "</div>";
             $first = false;
